e7c4cad32895f03b672afa7efdfed29eef97829b
Herich Tomas (U803909) <tomas.herich@mobi.ch>
2020-03-16 01:15:43 +0100

    feat(lib): improve architecture, add runner, use skip instead of error

---
+++ lib/tasks/testConnectionTask.ts
@@ -0,0 +1,20 @@
+import { ListrTask } from 'listr';
+
+import * as api from '../services/api.service';
+
+export const testConnectionTask: ListrTask = {
+  title: 'Test connection',
+  skip: ctx => {
+    if (!process.env.OMNIBOARD_API_KEY && !ctx.options.apiKey) {
+      return `Please provide --api-key argument or OMNIBOARD_API_KEY env variable`;
+    }
+  },
+  task: async (ctx, task) => {
+    const { organization } = await api.ping();
+    if (organization) {
+      task.title = `${task.title} successful, organization: ${organization}`;
+    } else {
+      throw new Error('Organization not found');
+    }
+  }
+};
