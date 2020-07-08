aae4cd6f9b4830a33c445bf6c851d8cde84be895
tomastrajan <tomas.trajan@gmail.com>
2020-07-08 11:36:42 +0200

    fix(env): handle both lower and uppercase proxy env variables

--- lib/index.ts
+++ lib/index.ts
@@ -2,6 +2,16 @@
 
 process.env.GLOBAL_AGENT_ENVIRONMENT_VARIABLE_NAMESPACE = '';
 
+if (process.env.http_proxy && !process.env.HTTP_PROXY) {
+  process.env.HTTP_PROXY = process.env.http_proxy;
+}
+if (process.env.https_proxy && !process.env.HTTPS_PROXY) {
+  process.env.HTTPS_PROXY = process.env.https_proxy;
+}
+if (process.env.no_proxy && !process.env.NO_PROXY) {
+  process.env.NO_PROXY = process.env.no_proxy;
+}
+
 import 'global-agent/bootstrap';
 import yargs, { Argv } from 'yargs';
 
