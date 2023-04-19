9a0463651dc9d47d33fb6626cba893c3a90c6dfc
tomastrajan <tomas.trajan@gmail.com>
2023-04-19 16:30:14 +0200

    fix: try to prevent OOM

--- lib/batch/tasks/init-job-repo.task.ts
+++ lib/batch/tasks/init-job-repo.task.ts
@@ -5,6 +5,7 @@ import {
   cloneRepo,
   pullLatest,
 } from '../../services/git.service';
+import { wait } from '../../utils/wait';
 import { Context } from '../../interface';
 import {
   directoryExists,
@@ -19,21 +20,26 @@ export function initJobRepo(job: string): ListrTask {
       const { workspacePath, verbose } = ctx.options;
       const repoName = getRepoNameFromUrl(job);
       const repoPath = pathJoin(workspacePath, repoName);
-      if (!directoryExists(repoPath)) {
-        await cloneRepo(job, workspacePath);
-        task.title = `${task.title}, cloned`;
-      } else {
-        try {
-          await pullLatest(repoPath);
-          task.title = `${task.title}, updated`;
-        } catch (error) {
-          task.title = `${task.title}, git pull failed, remove and retry to clone`;
-          removeDirectoryRecursive(repoPath);
+      try {
+        if (!directoryExists(repoPath)) {
           await cloneRepo(job, workspacePath);
           task.title = `${task.title}, cloned`;
+        } else {
+          try {
+            await pullLatest(repoPath);
+            task.title = `${task.title}, updated`;
+          } catch (error) {
+            task.title = `${task.title}, git pull failed, remove and retry to clone`;
+            removeDirectoryRecursive(repoPath);
+            await cloneRepo(job, workspacePath);
+            task.title = `${task.title}, cloned`;
+          }
         }
+        process.chdir(repoPath);
+      } catch (error: any) {
+        await wait();
+        throw new error();
       }
-      process.chdir(repoPath);
     },
   };
 }
